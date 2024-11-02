b6beffd339f56828afc809a73572aabaa89de5e1
Sanjaypranav <sanjaypriya195@.com>
2024-11-02 11:11:42 +0530

    Implement chatbot functionality with state management and remove unused tool module

--- kutty_ai/main.py
+++ kutty_ai/main.py
@@ -1,23 +1,37 @@
 import dotenv
 
-from langchain_community.tools.tavily_search import TavilySearchResults
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.runnables import Runnable
-
-# import boto3
-from typing import Annotated
-from typing_extensions import TypedDict
-from langgraph.graph.message import AnyMessage, add_messages
-
-from langchain_core.runnables import RunnableLambda
-from langgraph.prebuilt import ToolNode
-from langgraph.prebuilt import tools_condition
-
+from langchain_openai import ChatOpenAI
+from langgraph.graph import StateGraph, START, END
+from kutty_ai.agents import KuttyAssistant, State
+from kutty_ai.tools import call_sanjay_tool, call_subash_tool
 
 dotenv.load_dotenv()
+llm = ChatOpenAI(
+    model="gpt-4o-mini",
+)
 
+def chatbot(state: State) ->  dict:
+    return {"messages": llm.invoke(state["messages"])}
 
-
-
-
+#tools 
+tools = [
     
+]
+
+graph_builder = StateGraph(State)
+graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_edge(START, "chatbot")
+graph_builder.add_edge("chatbot", END)
+
+graph = graph_builder.compile()
+
+while True:
+  user_input=input("User: ")
+  if user_input.lower() in ["quit","q"]:
+    print("Good Bye")
+    break
+  for event in graph.stream({'messages':("user",user_input)}):
+    print(event.values())
+    for value in event.values():
+    #   print(value['messages'])
+      print("Assistant:",value["messages"].content)
